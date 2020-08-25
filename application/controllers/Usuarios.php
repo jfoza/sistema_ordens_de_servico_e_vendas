edca2ae644d6a8f9a5d1407124464399de45fac5
gfoza <gfozza@hotmail.com>
2020-08-25 13:09:57 -0300

    creation of tables in the database and users

--- application/controllers/Usuarios.php
+++ application/controllers/Usuarios.php
@@ -1,1 +1,25 @@
 <?php
+
+defined('BASEPATH') OR exit('Ação não permitida');
+
+class Usuarios extends CI_Controller {
+
+	public function __construct() {
+		parent::__construct();
+	}
+
+	public function index() {
+		$data = array(
+			'usuarios' => $this->ion_auth->users()->result(),
+		);
+
+//		echo '<pre>';
+//		print_r($data['usuarios']);
+//		exit();
+
+		$this->load->view('layout/header', $data);
+		$this->load->view('usuarios/index');
+		$this->load->view('layout/footer');
+
+	}
+}
